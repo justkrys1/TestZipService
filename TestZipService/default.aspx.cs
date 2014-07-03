245caba3401bcc4882efba046bfaf7fc3dde34b5
justkrys1 <justkrys1@gmail.com>
2014-07-03 11:03:20 -0700

    Test Zip Code Lookup
    Tests ASP.Net project Zip Code Lookup

---
+++ TestZipService/default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestZipService
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Zip2City1Client myClient = new ServiceReference1.Zip2City1Client();
+            lblCity.Text = myClient.GetCity(txtZip.Text);
+        }
+    }
+}
